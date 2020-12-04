0cd361dc727e023d5a8611f1e3f2b9e9a3312204
Eduardo Borges <euduardoborges@gmail.com>
2020-12-04 17:07:05 -0300

    refac: enhance prototype fns

--- src/classes.ts
+++ src/classes.ts
@@ -6,15 +6,13 @@ export interface ClassArray extends Array<Class> {}
 
 export type Class = string | number | ClassObject | ClassArray
 
-const isArray = Array.isArray
-
 export default function classes(obj: Class): string {
   var out = ''
 
   if (typeof obj === 'string' || typeof obj === 'number')
     return String(obj) || ''
 
-  if (isArray(obj)) {
+  if (Array.isArray(obj)) {
     for (var k = 0, tmp; k < obj.length; k++) {
       if ((tmp = classes(obj[k])) !== '') {
         out += (out && ' ') + tmp
@@ -22,8 +20,7 @@ export default function classes(obj: Class): string {
     }
   } else {
     for (const key in obj) {
-      const value = obj[key]
-      if (Object.prototype.hasOwnProperty.call(obj, key) && value) {
+      if (Object.prototype.hasOwnProperty.call(obj, key) && obj[key]) {
         out += (out && ' ') + key
       }
     }
